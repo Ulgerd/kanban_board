3ab1f99fe603899dcea13880e479b13b8db7ba55
DitRIX <melky@li.ru>
2019-05-22 22:58:43 +0300

    CSS update

--- src/utils/updateLists.js
+++
@@ -1,9 +0,0 @@
-import produce from "immer";
-
-export const updateLists = (tasksToDelete, lists, listID) => {
-  return produce(lists, draft => {
-    draft[listID].taskIDs = draft[listID].taskIDs.filter((task) =>
-      tasksToDelete.indexOf(task) < 0
-    ) // https://github.com/immerjs/immer/issues/115
-  })
-}
